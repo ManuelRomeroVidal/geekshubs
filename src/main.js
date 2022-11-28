6e4fd77e5bdefe91eee8326be1a5c2b6df2aaabd
Manuel Romero <manuelromerovidal@gmail.com>
2022-11-28 09:29:55 +0100

    add Boostrap

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+// Since every component imports their Bootstrap functionality,
+// the following line is not necessary:
+// import 'bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 var app = createApp(App).use(store).use(router);
 app.use(VueAxios, axios)
+app.use(BootstrapVue3)
 app.mount('#app');
